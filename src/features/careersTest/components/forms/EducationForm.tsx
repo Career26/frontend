65bd014e9a229f1c85b9d693d572ddb00d068144
Adam Milner <adam.milner@bp.com>
2023-07-19 12:24:17 +0100

    removes form

--- src/features/careersTest/components/forms/EducationForm.tsx
+++ src/features/careersTest/components/forms/EducationForm.tsx
@@ -13,30 +13,28 @@ type EducationFormProps = {
 export const EducationForm = ({ formik }: EducationFormProps) => (
   <div>
     Education
-    <form>
-      <FormText field="firstName" label="First Name" formik={formik} />
-      <FormText field="lastName" label="Last Name" formik={formik} />
-      Latest Degree
-      <UniversityForm formik={formik} baseField="latestDegree" />
-      Additional Degrees
-      <FieldArray name="additionalDegrees">
-        {({ push, remove }) => (
-          <div>
-            {formik.values.additionalDegrees?.map((degree, index) => (
-              <>
-                <h3>University {index + 1}</h3>
-                <UniversityForm
-                  key={`additional-degree-${degree.degreeName}`}
-                  baseField={`additionalDegrees[${index}]`}
-                  formik={formik}
-                />
-                <Button onClick={() => remove(index)}>Remove University</Button>
-              </>
-            ))}
-            <Button onClick={() => push({})}>Add Another University</Button>
-          </div>
-        )}
-      </FieldArray>
-    </form>
+    <FormText field="firstName" label="First Name" formik={formik} />
+    <FormText field="lastName" label="Last Name" formik={formik} />
+    Latest Degree
+    <UniversityForm formik={formik} baseField="latestDegree" />
+    Additional Degrees
+    <FieldArray name="additionalDegrees">
+      {({ push, remove }) => (
+        <div>
+          {formik.values.additionalDegrees?.map((degree, index) => (
+            <>
+              <h3>University {index + 1}</h3>
+              <UniversityForm
+                key={`additional-degree-${index}`}
+                baseField={`additionalDegrees[${index}]`}
+                formik={formik}
+              />
+              <Button onClick={() => remove(index)}>Remove University</Button>
+            </>
+          ))}
+          <Button onClick={() => push({})}>Add Another University</Button>
+        </div>
+      )}
+    </FieldArray>
   </div>
 );
--- src/features/careersTest/components/forms/PreviousExperienceForm.tsx
+++ src/features/careersTest/components/forms/PreviousExperienceForm.tsx
@@ -12,23 +12,21 @@ type PreviousExperienceFormProps = {
 export const PreviousExperienceForm = ({ formik }: PreviousExperienceFormProps) => (
   <div>
     Previous Experiences
-    <form>
-      <FieldArray name="previousWorkExperience">
-        {({ push, remove }) => (
-          <div>
-            <h3>Company 1</h3>
-            <CompanyForm key="company-1" formik={formik} index={0} />
-            {formik.values.previousWorkExperience?.map((_, index) => (
-              <>
-                <h3>Company {index + 2}</h3>
-                <CompanyForm key={`company-${index}`} formik={formik} index={index + 1} />
-                <Button onClick={() => remove(index)}>Remove Company</Button>
-              </>
-            ))}
-            <Button onClick={() => push({})}>Add Another Company</Button>
-          </div>
-        )}
-      </FieldArray>
-    </form>
+    <FieldArray name="previousWorkExperience">
+      {({ push, remove }) => (
+        <div>
+          <h3>Company 1</h3>
+          <CompanyForm key="company-1" formik={formik} index={0} />
+          {formik.values.previousWorkExperience?.map((_, index) => (
+            <>
+              <h3>Company {index + 2}</h3>
+              <CompanyForm key={`company-${index}`} formik={formik} index={index + 1} />
+              <Button onClick={() => remove(index)}>Remove Company</Button>
+            </>
+          ))}
+          <Button onClick={() => push({})}>Add Another Company</Button>
+        </div>
+      )}
+    </FieldArray>
   </div>
 );

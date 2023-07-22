59d44a9a3133b532fc6bfd7750845e80c2b334dd
Adam Milner <adam.milner@bp.com>
2023-07-22 19:22:34 +0100

    adds active color

--- src/shared/components/buttons/Button.tsx
+++ src/shared/components/buttons/Button.tsx
@@ -2,16 +2,29 @@ import MuiButton from '@mui/material/Button';
 import React from 'react';
 
 import './buttonStyles.scss';
+import classNames from 'classnames';
 
 type ButtonProps = {
   variant?: 'outlined' | 'contained';
   onClick?: () => void;
   disabled?: boolean;
   label: string;
+  active?: boolean;
 };
 
-export const Button = ({ label, onClick, disabled, variant = 'contained' }: ButtonProps) => (
-  <MuiButton onClick={onClick} variant={variant} disabled={disabled} className="buttons">
+export const Button = ({
+  label,
+  onClick,
+  active,
+  disabled,
+  variant = 'contained',
+}: ButtonProps) => (
+  <MuiButton
+    onClick={onClick}
+    disabled={disabled}
+    variant={variant}
+    className={classNames('buttons', { active })}
+  >
     {label}
   </MuiButton>
 );

e6b18c17828d5d861b68e3e5d331f39598ae7e8b
Adam Milner <amilner568@gmail.com>
2023-09-29 09:24:14 +0100

    auth check

--- src/shared/hooks/useSession.ts
+++ src/shared/hooks/useSession.ts
@@ -3,18 +3,26 @@ import { selectLoginModal, setLoginModal } from '@slices/userSlice';
 import { useAppDispatch, useAppSelector } from '@state/store';
 import { useEffect } from 'react';
 
+import useCareerNavigation from './useCareerNavigation';
+
 export const useSession = () => {
   const dispatch = useAppDispatch();
   const { open } = useAppSelector(selectLoginModal);
-  const { user, signOut } = useAuthenticator((context) => [context.route]);
+  const { signOut, user, authStatus } = useAuthenticator((context) => [context.route]);
+  const { clickLogo } = useCareerNavigation();
+  const authenticated = authStatus === 'authenticated';
 
   useEffect(() => {
+    if (!authenticated) {
+      clickLogo();
+    }
     if (open) {
       dispatch(setLoginModal({ open: false }));
     }
-  }, [user]);
+  }, [authenticated, signOut]);
 
   return {
+    authenticated,
     user,
     signOut,
   };

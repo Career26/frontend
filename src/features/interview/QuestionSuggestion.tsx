6ee55f79f9de537b2a2d243199af4128b30e0544
Adam Milner <amilner568@gmail.com>
2023-10-16 21:44:37 +0100

    v2

--- src/features/interview/QuestionSuggestion.tsx
+++ src/features/interview/QuestionSuggestion.tsx
@@ -1,5 +1,15 @@
 import { useGetSuggestionMutation } from '@apis/questionsApi';
-import { Accordion, List, Loader, Paper, Text, ThemeIcon, createStyles, rem } from '@mantine/core';
+import {
+  Accordion,
+  Badge,
+  List,
+  Loader,
+  Paper,
+  Text,
+  ThemeIcon,
+  createStyles,
+  rem,
+} from '@mantine/core';
 import { selectSelectedCareerPathId, selectSelectedQuestion } from '@slices/sessionSlice';
 import { useAppSelector } from '@state/store';
 import classNames from 'classnames';
@@ -21,21 +31,43 @@ const suggestionStyles = createStyles({
   },
 });
 
+const NumberedList = ({ items }: { items: string[] }) =>
+  items.map((item, index) => (
+    <List.Item key={`suggestion-${item}`} icon={<ThemeIcon radius="xl">{index + 1}</ThemeIcon>}>
+      {item}
+    </List.Item>
+  ));
+
+const StarList = ({ starMap }: { starMap: { [key: string]: string } }) =>
+  Object.entries(starMap).map(([key, value]) => (
+    <List.Item key={`suggestion-${key}`} icon={<Badge>{key}</Badge>}>
+      {value}
+    </List.Item>
+  ));
+
+const getStarMap = (suggestedFormat: string) => {
+  const regex = /Situation: (.*?) Task: (.*?) Action: (.*?) Result: (.*?)$/;
+  const match = suggestedFormat.match(regex);
+  if (!match) {
+    return undefined;
+  }
+  return {
+    Situation: match[1].trim(),
+    Task: match[2].trim(),
+    Action: match[3].trim(),
+    Result: match[4].trim(),
+  };
+};
+
 const SuggestedFormat = ({ suggestedFormat }: { suggestedFormat: string }) => {
   const { classes } = suggestionStyles();
-  const items = suggestedFormat.split(/(\\n)?\d\. /gm).filter(Boolean);
+  const starMap = getStarMap(suggestedFormat);
+  const numberedItems = suggestedFormat.split(/(\\n)?\d\. /gm).filter(Boolean);
   return (
     <div>
       <Text weight="bold">Suggested Format:</Text>
       <List spacing="md" center className={classes.list}>
-        {items.map((item, index) => (
-          <List.Item
-            key={`suggestion-${item}`}
-            icon={<ThemeIcon radius="xl">{index + 1}</ThemeIcon>}
-          >
-            {item}
-          </List.Item>
-        ))}
+        {starMap ? <StarList starMap={starMap} /> : <NumberedList items={numberedItems} />}
       </List>
     </div>
   );
@@ -57,12 +89,15 @@ export const QuestionSuggestion = () => {
     useGetSuggestionMutation();
 
   useEffect(() => {
-    setValue(null);
-    if (!selectedQuestion) {
+    if (!selectedQuestion || !value) {
       return;
     }
-    getSuggestion({ careerPathId, question: selectedQuestion.question });
-  }, [selectedQuestion]);
+    getSuggestion({ question: selectedQuestion.question, careerPathId });
+  }, [value, selectedQuestion]);
+
+  useEffect(() => {
+    setValue(null);
+  }, [selectedQuestion, careerPathId]);
 
   return (
     <Paper h="auto" withBorder shadow="md">
@@ -75,9 +110,9 @@ export const QuestionSuggestion = () => {
             ) : (
               suggestion && (
                 <div className={classes.container}>
-                  <SuggestedFormat suggestedFormat={suggestion.suggestedFormat} />
-                  <TextBlock title="Sample Answer" content={suggestion.sampleAnswer} />
-                  <TextBlock title="Reasoning" content={suggestion.whySuitable} />
+                  <SuggestedFormat suggestedFormat={suggestion?.suggestedFormat} />
+                  <TextBlock title="Sample Answer" content={suggestion?.sampleAnswer} />
+                  <TextBlock title="Reasoning" content={suggestion?.whySuitable} />
                 </div>
               )
             )}

938215168d02e1b07f63c679eeb7ee51037c8550
Adam Milner <amilner568@gmail.com>
2023-10-18 21:58:57 +0100

    temp

--- src/features/careerTest/components/careerPathsForm/CareerPathsForm.tsx
+++ src/features/careerTest/components/careerPathsForm/CareerPathsForm.tsx
@@ -1,4 +1,4 @@
-import { Text, Grid, Container } from '@mantine/core';
+import { Text, Grid, Container, createStyles } from '@mantine/core';
 import React, { useEffect } from 'react';
 import { formStyles } from '@shared/styles/formStyles';
 import { useAppDispatch, useAppSelector } from '@state/store';
@@ -10,6 +10,12 @@ import { UserProfile } from '@datatypes/profile';
 
 import { CareerPathActions } from './CareerPathActions';
 
+const careerPathsStyles = createStyles({
+  tile: {
+    width: '100vh',
+  },
+});
+
 export const CareerPathsForm = ({
   careerPaths,
   profileId,
@@ -17,7 +23,8 @@ export const CareerPathsForm = ({
   profileId?: string;
   careerPaths?: UserProfile['careerPaths'];
 }) => {
-  const { classes } = formStyles();
+  const { classes } = careerPathsStyles();
+  const { classes: formClasses } = formStyles();
   const industryColors = useAppSelector(selectIndustryColors);
   const { selectedCareers, toggleSelectedCareer, loadingCareers } = useCareerSelection();
   const dispatch = useAppDispatch();
@@ -29,12 +36,12 @@ export const CareerPathsForm = ({
 
   return (
     <Shell>
-      <Container className={classes.questionContainer}>
-        <Text className={classes.questionTitle}>Career Paths</Text>
-        <Text className={classes.subHeader}>Select the careers that you like</Text>
+      <Container className={formClasses.questionContainer}>
+        <Text className={formClasses.questionTitle}>Career Paths</Text>
+        <Text className={formClasses.subHeader}>Select the careers that you like</Text>
         <Grid>
           {Object.entries(careerPaths || {}).map(([careerId, careerPath]) => (
-            <Grid.Col md={6} key={`career-path-${careerId}`}>
+            <Grid.Col md={6} key={`career-path-${careerId}`} className={classes.tile}>
               <CareerCard
                 title={careerPath.title}
                 subTitle={careerPath.startingSalary}

1efc89e5493f4d43dd1224a1f157c4aaabb60a64
Adam Milner <amilner568@gmail.com>
2023-10-18 22:54:31 +0100

    example

--- src/shared/components/loadingScreen/LoadingScreen.tsx
+++ src/shared/components/loadingScreen/LoadingScreen.tsx
@@ -1,68 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import { Image } from '@mantine/core';
-import magnifyingGlass from '@assets/magnifyingGlass.svg';
+import React, { useState } from 'react';
 
 import './loadingScreen.scss';
 import './bounce.css';
-
-const Typewriter = ({
-  text,
-  textDelay,
-  repeatDelay,
-  deleteDelay,
-  onComplete,
-}: {
-  text: string;
-  textDelay: number;
-  repeatDelay: number;
-  deleteDelay: number;
-  onComplete?: () => void;
-}) => {
-  const [currentText, setCurrentText] = useState('');
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const [isComplete, setIsComplete] = useState(false);
-
-  useEffect(() => {
-    if (currentIndex === -1 && onComplete) {
-      onComplete();
-    }
-  }, [currentIndex]);
-
-  useEffect(() => {
-    if (currentIndex === text.length && !isComplete) {
-      const timeout = setTimeout(() => {
-        setIsComplete(true);
-      }, deleteDelay);
-      return () => clearTimeout(timeout);
-    }
-    if (currentIndex < text.length && !isComplete) {
-      const timeout = setTimeout(() => {
-        setCurrentText((prevText) => prevText + text[currentIndex]);
-        setCurrentIndex((prevIndex) => prevIndex + 1);
-      }, textDelay);
-      return () => clearTimeout(timeout);
-    }
-    if (currentIndex > -1) {
-      const timeout = setTimeout(() => {
-        setCurrentText((prevText) => prevText.slice(0, -1));
-        setCurrentIndex((prevIndex) => prevIndex - 1);
-      }, textDelay);
-      return () => clearTimeout(timeout);
-    }
-    if (currentIndex === -1) {
-      const timeout = setTimeout(() => {
-        setIsComplete(false);
-        setCurrentIndex(0);
-      }, repeatDelay);
-      return () => clearTimeout(timeout);
-    }
-    return () => {};
-  }, [currentIndex, repeatDelay, textDelay, text, isComplete, deleteDelay]);
-
-  return <span>{currentText}</span>;
-};
-
-export default Typewriter;
+import { Typewriter } from './TypeWriter';
 
 export const LoadingScreen = () => {
   const lines = ['line 1...', 'item 2...', 'dave...'];
@@ -75,10 +15,9 @@ export const LoadingScreen = () => {
   return (
     <div className="container">
       <div className="bounceContainer">
-        <div className="logo" />
+        <div className="logo">LOGO HERE</div>
         <div className="shadow" />
-      </div>
-      <div>
+
         <h1>
           <Typewriter
             text={lines[lineIndex] || ''}

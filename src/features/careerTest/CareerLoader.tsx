09bee4d90075fae7bd34da672611afdfcd9966e6
Adam Milner <amilner568@gmail.com>
2023-10-24 21:08:41 +0100

    updates loading positin

--- src/features/careerTest/CareerLoader.tsx
+++ src/features/careerTest/CareerLoader.tsx
@@ -6,29 +6,31 @@ import styles from './careerTestStyles.module.scss';
 
 export const CareerLoader = () => (
   <>
-    <TypeWriter
-      repeatSequence
-      text={[
-        {
-          text: 'Generating your personalised career suggestions...',
-          textDelay: 40,
-          repeatDelay: 1000,
-          deleteDelay: 2000,
-        },
-        {
-          text: 'This may take up to 30 seconds...',
-          textDelay: 40,
-          repeatDelay: 1000,
-          deleteDelay: 2000,
-        },
-        {
-          text: "You're almost there...",
-          textDelay: 40,
-          repeatDelay: 1000,
-          deleteDelay: 2000,
-        },
-      ]}
-    />
+    <div className={styles.typeWriter}>
+      <TypeWriter
+        repeatSequence
+        text={[
+          {
+            text: 'Generating your personalised career suggestions...',
+            textDelay: 40,
+            repeatDelay: 1000,
+            deleteDelay: 2000,
+          },
+          {
+            text: 'This may take up to 30 seconds...',
+            textDelay: 40,
+            repeatDelay: 1000,
+            deleteDelay: 2000,
+          },
+          {
+            text: "You're almost there...",
+            textDelay: 40,
+            repeatDelay: 1000,
+            deleteDelay: 2000,
+          },
+        ]}
+      />
+    </div>
     <div className={styles.loader}>
       <Loader type="dots" size="xl" />
     </div>

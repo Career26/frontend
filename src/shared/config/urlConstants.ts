fc81cd9767fa6a7d63d80d651cebb16680183822
Adam Milner <amilner568@gmail.com>
2023-10-29 22:49:44 +0000

    updates

--- src/shared/config/urlConstants.ts
+++ src/shared/config/urlConstants.ts
@@ -7,4 +7,9 @@ export const urls = {
   settings: '/settings',
 };
 
-export const baseUrl = 'https://qjop4yl84g.execute-api.eu-west-1.amazonaws.com/Prod';
+const devUrl = 'https://qjop4yl84g.execute-api.eu-west-1.amazonaws.com/Prod';
+const prodUrl = 'https://4gsxsq4qkb.execute-api.eu-west-1.amazonaws.com/Prod';
+
+const getBaseUrl = () => (process.env.NODE_ENV === 'production' ? prodUrl : devUrl);
+
+export const baseUrl = getBaseUrl();

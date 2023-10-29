3ca3c5dfd91f84eb6e101766e5bc8fd089d9c4ce
Adam Milner <amilner568@gmail.com>
2023-10-29 14:03:04 +0000

    test updates

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,8 +1,9 @@
 import { http, HttpResponse } from 'msw';
 import { setupServer } from 'msw/node';
+import { baseUrl } from '@shared/config/urlConstants';
 
 import { mockUserProfile } from './profileMocks';
 
-export const handlers = [http.get('/profile', () => HttpResponse.json(mockUserProfile))];
+export const handlers = [http.get(`${baseUrl}/profile`, () => HttpResponse.json(mockUserProfile))];
 
 export const server = setupServer(...handlers);
--- src/shared/utils/testUtil.tsx
+++ src/shared/utils/testUtil.tsx
@@ -1,13 +1,18 @@
 import { render } from '@testing-library/react';
 import type { RenderOptions } from '@testing-library/react';
-import { PreloadedState, configureStore } from '@reduxjs/toolkit';
+import { PreloadedState } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
 import React, { PropsWithChildren } from 'react';
-import overview, { overviewApi } from '@apis/overviewApi';
-import profile, { profileApi } from '@apis/profileApi';
-import questions, { questionsApi } from '@apis/questionsApi';
-import session, { initialSessionState } from '@slices/sessionSlice';
+import { overviewApi } from '@apis/overviewApi';
+import { profileApi } from '@apis/profileApi';
+import { questionsApi } from '@apis/questionsApi';
+import { initialSessionState } from '@slices/sessionSlice';
 import { AppStore, RootState, store as stateStore } from '@state/store';
+
+interface ExtendedRenderOptions extends Omit<RenderOptions, 'queries'> {
+  preloadedState?: PreloadedState<RootState>;
+  store?: AppStore;
+}
 
 export const testState: RootState = {
   session: initialSessionState,
@@ -16,27 +21,17 @@ export const testState: RootState = {
   [profileApi.reducerPath]: profileApi.reducer(undefined, { type: 'profile' }),
 };
 
-interface ExtendedRenderOptions extends Omit<RenderOptions, 'queries'> {
-  preloadedState?: PreloadedState<RootState>;
-  store?: AppStore;
-}
-
 export const Wrapper = ({ children }: PropsWithChildren) => (
   <Provider store={stateStore}>{children}</Provider>
 );
 
 export const renderWithProviders = (
   ui: React.ReactElement,
-  {
-    preloadedState = {},
-    // Automatically create a store instance if no store was passed in
-    store = configureStore({ reducer: { session, profile, overview, questions }, preloadedState }),
-    ...renderOptions
-  }: ExtendedRenderOptions = {},
+  renderOptions: ExtendedRenderOptions = {},
 ) => {
   const SubWrapper = ({ children }: PropsWithChildren): JSX.Element => (
-    <Provider store={store}>{children}</Provider>
+    <Provider store={stateStore}>{children}</Provider>
   );
   // Return an object with the store and all of RTL's query functions
-  return { store, ...render(ui, { wrapper: SubWrapper, ...renderOptions }) };
+  return { store: stateStore, ...render(ui, { wrapper: SubWrapper, ...renderOptions }) };
 };

c86f75d0f32a6146d45fd6212b754c435eabba2a
Adam Milner <amilner568@gmail.com>
2023-11-05 23:35:32 +0000

    updates label

--- src/features/careerTest/components/workExperienceForm/ExperienceForm.tsx
+++ src/features/careerTest/components/workExperienceForm/ExperienceForm.tsx
@@ -58,7 +58,7 @@ export const ExperienceForm = ({
       <Select
         {...form.getInputProps(`${baseKey}.rating`)}
         withAsterisk
-        label="Rating"
+        label="What did you think of this role?"
         data={ratingOptions}
         placeholder="Select a rating"
         py="xs"

f62a0f8c30db44e35185f7b91f2b39424bab98cb
Adam Milner <amilner568@gmail.com>
2023-11-16 23:28:11 +0000

    login

--- src/shared/components/display/TextWithIconBlock.tsx
+++ src/shared/components/display/TextWithIconBlock.tsx
@@ -1,6 +1,8 @@
 import { Group, Text } from '@mantine/core';
 import React from 'react';
 import commonStyles from '@shared/styles/commonStyles.module.scss';
+import { useMobileStyles } from '@shared/hooks/useMobileStyles';
+import classNames from 'classnames';
 
 export const TextWithIconBlock = ({
   title,
@@ -10,14 +12,22 @@ export const TextWithIconBlock = ({
   title: string;
   content: React.ReactNode;
   Icon: React.ReactNode;
-}) => (
-  <div>
-    <Group className={commonStyles.row}>
-      {Icon}
-      <Text fw={800} size="1.5rem" py="md">
-        {title}
-      </Text>
-    </Group>
-    {content}
-  </div>
-);
+}) => {
+  const { isMobile } = useMobileStyles();
+  return (
+    <div>
+      <Group
+        className={classNames({
+          [commonStyles.row]: !isMobile,
+          [commonStyles.rowMobile]: isMobile,
+        })}
+      >
+        {Icon}
+        <Text fw={800} size={isMobile ? '1rem' : '1.5rem'} py="md">
+          {title}
+        </Text>
+      </Group>
+      {content}
+    </div>
+  );
+};

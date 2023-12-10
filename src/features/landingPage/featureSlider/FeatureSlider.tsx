f699bbe0f2f545dcacc098c5b416b91797d4468c
Adam Milner <amilner568@gmail.com>
2023-12-10 20:43:05 +0000

    udpates to landing slider

--- src/features/landingPage/featureSlider/FeatureSlider.tsx
+++ src/features/landingPage/featureSlider/FeatureSlider.tsx
@@ -1,7 +1,7 @@
 import React, { useRef } from 'react';
 import Autoplay from 'embla-carousel-autoplay';
 import { Carousel } from '@mantine/carousel';
-import { Container, Image, Text, Title } from '@mantine/core';
+import { Container, Image, Title } from '@mantine/core';
 import careerResultsImage from '@assets/careerResults.png';
 import interviewImage from '@assets/interviewQuestion.png';
 import careerTestImage from '@assets/careerTest.png';
@@ -14,33 +14,26 @@ const slides = [
   {
     title: 'Take Our Free',
     subTitle: 'Career Test',
-    description: "Our career test is easy to use, quick to complete, and best of all it's free!",
     image: careerTestImage,
   },
   {
     title: 'Find Your',
     subTitle: 'Perfect Career',
-    description: 'Receive tailored career suggestions and discover your perfect career path.',
     image: careerResultsImage,
   },
   {
     title: 'Gain Industry',
     subTitle: 'Insights',
-    description: 'Explore indsutry insights and what its really like to work in your dream career',
     image: industryInsightsImage,
   },
   {
     title: 'Interview With',
     subTitle: 'Confidence',
-    description:
-      'Our interactive interview questions prepare you for even the most challenging interviews.',
     image: interviewImage,
   },
   {
     title: 'Connect With',
     subTitle: 'Industry Mentors',
-    description:
-      'Join our network and receive mentoring from industry professions who know the secrets to success.',
     image: networkImg,
   },
 ];
@@ -58,22 +51,13 @@ export const FeatureSlider = () => {
         onMouseEnter={autoplay.current.stop}
         onMouseLeave={autoplay.current.reset}
       >
-        {slides.map(({ title, subTitle, description, image }) => (
+        {slides.map(({ title, subTitle, image }) => (
           <Carousel.Slide key={`slide-${title}`} className={styles.slide}>
-            <Container px="md" className={styles.titleContainer}>
-              <Title className={styles.title}>
-                {title}{' '}
-                <Text component="span" c="navy" inherit>
-                  {subTitle}
-                </Text>
-              </Title>
-              <Text c="dimmed" py="md" className={styles.description}>
-                {description}
-              </Text>
-            </Container>
-            <Container>
-              <Image src={image} radius="md" />
-            </Container>
+            <div className={styles.title}>
+              <Title>{title} </Title>
+              <Title c="navy">{subTitle}</Title>
+            </div>
+            <Image src={image} radius="md" />
           </Carousel.Slide>
         ))}
       </Carousel>

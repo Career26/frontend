5b2aa0d55f873bb9eaef621a7cef140ea0d33f66
AMilner7 <98622246+AMilner7@users.noreply.github.com>
2023-12-10 21:21:57 +0000

    Adds diversity form (#111)

--- src/features/careerTest/components/diversityForm/DiversityForm.tsx
+++ src/features/careerTest/components/diversityForm/DiversityForm.tsx
@@ -1,13 +1,11 @@
 import { CareerFormProps } from '@careerTest/careerTestTypes';
 import { Accordion, Container, Select } from '@mantine/core';
-import { DateInput } from '@mantine/dates';
 import React from 'react';
 import { YesNoPreferNotToSay, Ethnicity, Gender, SchoolType } from '@datatypes/profile';
 import commonStyles from '@shared/styles/commonStyles.module.scss';
 import { IconInfoCircle } from '@tabler/icons-react';
 
 import { FormContent } from '../FormContent';
-import { SelectHousehold } from './SelectHousehold';
 
 const yesNoPreferOptions = Object.entries(YesNoPreferNotToSay).map(([label, value]) => ({
   label,
@@ -24,13 +22,10 @@ export const DiversityForm = ({ form }: { form: CareerFormProps }) => (
               Why do we ask for this information?
             </Accordion.Control>
             <Accordion.Panel>
-              To get the most accurate results, these answers can be used to:
+              We use the answer to:
               <ul>
-                <li>Provide insights into careers you may not have considered</li>
-                <li>Suggest tailored opportunities based on your background</li>
-                <li>
-                  Start building your mentor network to connect with users with similar backgrounds
-                </li>
+                <li>Provide you with career options you may not have considered</li>
+                <li>Match you with mentors from shared backgrounds</li>
               </ul>
             </Accordion.Panel>
           </Accordion.Item>
@@ -70,19 +65,6 @@ export const DiversityForm = ({ form }: { form: CareerFormProps }) => (
           label="What is your current gender identity?"
           data={Object.entries(Gender).map(([label, value]) => ({ label, value }))}
           searchable
-        />
-      </div>
-      <div className={commonStyles.row}>
-        <SelectHousehold form={form} />
-      </div>
-      <div className={commonStyles.row}>
-        <DateInput
-          {...form.getInputProps('diversity.age')}
-          label="What is your date of birth?"
-          maxDate={new Date()}
-          clearable
-          placeholder="YYYY-MM-DD"
-          valueFormat="YYYY-MM-DD"
         />
       </div>
     </FormContent>

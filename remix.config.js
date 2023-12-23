b2b124953f6a493a7e2289a4e911fd2f29a41cb5
Adam Milner <amilner568@gmail.com>
2023-12-23 11:21:01 +0000

    updates

--- remix.config.js
+++ remix.config.js
@@ -3,4 +3,15 @@ export default {
   postcss: true,
   ignoredRouteFiles: ['**/.*'],
   browserNodeBuiltinsPolyfill: { modules: { url: true, buffer: true } },
+  serverDependenciesToBundle: [
+    'react',
+    'node:stream',
+    '@remix-run/node',
+    '@remix-run/react',
+    'isbot',
+    'react-dom/server',
+    'react-jsx-runtime',
+    /@mantine/,
+    '@aws-amplify/ui-react',
+  ],
 };
